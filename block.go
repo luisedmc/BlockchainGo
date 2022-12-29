0c19199ef4b85a270981020a1ef1144340ba1c5e
LuisEduardo-M <luiseduardomc019@gmail.com>
2022-12-29 11:34:00 -0300

    Creating a new Block function

--- block.go
+++ block.go
@@ -1,8 +1,36 @@
 package main
 
+import (
+	"bytes"
+	"crypto/sha256"
+)
+
 // Block represents each 'item' in the blockchain
 type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
+}
+
+// DeriveHash derives a hash from the block's data
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{
+		b.Data,
+		b.PrevHash,
+	}, []byte{})
+	hash := sha256.Sum256(info)
+
+	b.Hash = hash[:]
+}
+
+// CreateBlock creates and returns a new Block
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{
+		Hash:     []byte{},
+		Data:     []byte(data),
+		PrevHash: prevHash,
+	}
+	block.DeriveHash()
+
+	return block
 }
