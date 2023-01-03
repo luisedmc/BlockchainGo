3d0f7b47cf3d764de3f2f286a71c039d4b40ec9c
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-01-02 21:39:09 -0300

    Initializing the Blockchain with the Database

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,10 +1,21 @@
 package blockchain
 
+import (
+	"fmt"
+
+	"github.com/dgraph-io/badger/v3"
+)
+
+const (
+	dbPath = "./tmp/blocks"
+)
+
 type Blockchain struct {
-	Blocks []*Block
+	LastHash []byte
+	Database *badger.DB
 }
 
-// AddBlock adds a block to the blockchain
+// AddBlock adds a block to the blockchain.
 func (chain *Blockchain) AddBlock(data string) {
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
 
@@ -13,14 +24,54 @@ func (chain *Blockchain) AddBlock(data string) {
 	chain.Blocks = append(chain.Blocks, newBlock)
 }
 
-// Genesis is the first block of the Blockchain
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-// InitBlockchain starts the Blockchain
+// InitBlockchain creates a new Blockchain with genesis Block.
 func InitBlockchain() *Blockchain {
-	return &Blockchain{
-		Blocks: []*Block{Genesis()},
+	var lastHash []byte
+
+	opts := badger.DefaultOptions(dbPath)
+	opts.Dir = dbPath
+	opts.ValueDir = dbPath
+
+	// Opening Database
+	db, err := badger.Open(opts)
+	HandleErrors(err)
+
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("No existing Blockchain found...")
+
+			genesis := Genesis()
+			fmt.Println("Genesis Proved")
+
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			HandleErrors(err)
+
+			err = txn.Set([]byte("lh"), genesis.Hash)
+
+			lastHash = genesis.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			HandleErrors(err)
+
+			err = item.Value(func(val []byte) error {
+				lastHash = append([]byte{}, val...)
+
+				return nil
+			})
+			HandleErrors(err)
+
+			return err
+		}
+	})
+
+	HandleErrors(err)
+
+	blockchain := Blockchain{
+		LastHash: lastHash,
+		Database: db,
 	}
+
+	return &blockchain
 }
