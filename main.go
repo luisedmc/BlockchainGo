96ebd4a0a60c979c587fd3871ff21bbc877da996
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-02-07 15:43:54 -0300

    initializing the blockchain and printing the blocks

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 	// Blockchain
-	// Block
+	chain := InitBlockchain()
+
+	chain.AddBlock("First Block after Genesis Block.")
+	chain.AddBlock("Second Block after Genesis Block.")
+	chain.AddBlock("Third Block after Genesis Block.")
+
+	// Printing Blocks in the Blockchain
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Block Data: %s\n", block.Data)
+		fmt.Printf("Block Hash: %x\n\n", block.Hash)
+	}
+
 	// Transaction
-	// Wallet
+
 	// Proof of Work
+
+	// Wallet
+
 }
