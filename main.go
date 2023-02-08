8f6087dffd24b8b268e12f0ab28a51dce94df0d1
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-02-08 10:42:19 -0300

    testing a proof of work

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	// Blockchain
@@ -11,15 +15,18 @@ func main() {
 	chain.AddBlock("Third Block after Genesis Block.")
 
 	// Printing Blocks in the Blockchain
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Block Data: %s\n", block.Data)
 		fmt.Printf("Block Hash: %x\n\n", block.Hash)
+
+		// Proof of Work
+		pow := NewProofOfWork(block)
+		fmt.Printf("Proof of Work: %s\n", strings.Title(strconv.FormatBool(pow.Validate())))
+		fmt.Println()
 	}
 
 	// Transaction
-
-	// Proof of Work
 
 	// Wallet
 
