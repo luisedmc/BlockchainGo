a72c43ab354a5253e1e58b28616cdfdc55844897
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-02-08 10:43:44 -0300

    formatting

--- blockchain.go
+++ blockchain.go
@@ -2,22 +2,17 @@ package main
 
 // Blockchain struct holds all the blocks in the blockchain
 type Blockchain struct {
-	blocks []*Block
-}
-
-// InitBlockchain initializes the blockchain
-func InitBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{Genesis()}}
-}
-
-// Genesis creates the first block in the blockchain
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
+	Blocks []*Block
 }
 
 // AddBlock adds a new block to the blockchain
 func (chain *Blockchain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
 	newBlock := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, newBlock)
+	chain.Blocks = append(chain.Blocks, newBlock)
+}
+
+// InitBlockchain initializes the blockchain
+func InitBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
 }
