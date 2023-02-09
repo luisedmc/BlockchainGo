9e36853f141cfb15af1bac1fbc9c8cab3a6af630
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-02-09 16:05:18 -0300

    refactoring the blockchain to start using the database badger and adding the blocks to the blockchain

--- blockchain.go
+++ blockchain.go
@@ -1,18 +1,130 @@
 package main
 
-// Blockchain struct holds all the blocks in the blockchain
+import (
+	"fmt"
+	"log"
+
+	"github.com/dgraph-io/badger/v3"
+)
+
+// Blockchain interacts with the Database
 type Blockchain struct {
-	Blocks []*Block
+	LastHash []byte
+	Database *badger.DB
 }
 
-// AddBlock adds a new block to the blockchain
-func (chain *Blockchain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, newBlock)
-}
+// Path to save blocks in the Database
+const (
+	dbPath = "./tmp/blocks"
+)
 
-// InitBlockchain initializes the blockchain
+// InitBlockchain initializes the Blockchain Database
 func InitBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	var lastHash []byte
+
+	// Defining options for the Database
+	opts := badger.DefaultOptions(dbPath)
+	opts.Dir = dbPath
+	opts.ValueDir = dbPath
+
+	// Opening BadgerDB with options
+	db, err := badger.Open(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Update(func(txn *badger.Txn) error {
+		// Checking if already exists a blockchain in the database by trying to find a last hash (lh)
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("Blockchain not found in the database.")
+
+			genesis := NewGenesisBlock()
+			fmt.Println("Genesis created.")
+
+			// Saving Genesis Block
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = txn.Set([]byte("lh"), genesis.Hash)
+			lastHash = genesis.Hash
+
+			return err
+
+		} else {
+			// Retrieving data
+			fmt.Println("Blockchain not found in the database.")
+
+			item, err := txn.Get([]byte("lh"))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = item.Value(func(val []byte) error {
+				lastHash = append([]byte{}, val...)
+
+				return nil
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			return err
+		}
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Defining the Blockchain
+	blockchain := Blockchain{
+		LastHash: lastHash,
+		Database: db,
+	}
+
+	return &blockchain
+}
+
+// AddBlock adds a new Block to the Blockchain
+func (chain *Blockchain) AddBlock(data string) {
+	var lastHash []byte
+
+	// To add a new Block, we get the last block hash from the Database to use it to mine a new Block hash
+	err := chain.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = item.Value(func(val []byte) error {
+			lastHash = append([]byte{}, val...)
+
+			return nil
+		})
+
+		return err
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newBlock := CreateBlock(data, lastHash)
+
+	// Updating the Database with the newBlock
+	err = chain.Database.Update(func(txn *badger.Txn) error {
+		err := txn.Set(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = txn.Set([]byte("lh"), newBlock.Hash)
+
+		chain.LastHash = newBlock.Hash
+
+		return err
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
