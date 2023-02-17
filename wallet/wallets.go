cc4ad7f1328c9ef0f73a14b1d0a8212b20be62cb
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-02-17 09:57:15 -0300

    feat: adding basic functionalities for multiple wallets (save, load file, create, get)

---
+++ wallet/wallets.go
@@ -0,0 +1,78 @@
+package wallet
+
+import (
+	"bytes"
+	"crypto/elliptic"
+	"encoding/gob"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+const walletFile = "./tmp/wallets.data"
+
+// Wallets holds multiple Wallets
+type Wallets struct {
+	Wallets map[string]*Wallet
+}
+
+// CreateWallets creates Wallets and fill it's content from an existent file
+func CreateWallets() (*Wallets, error) {
+	wallets := Wallets{}
+	wallets.Wallets = make(map[string]*Wallet)
+
+	err := wallets.LoadFile()
+
+	return &wallets, err
+}
+
+// GetWallet gets a specific Wallet by address
+func (ws Wallets) GetWallet(address string) Wallet {
+	return *ws.Wallets[address]
+}
+
+// SaveToFile saves Wallets to a file
+func (ws *Wallets) SaveToFile() {
+	var content bytes.Buffer
+
+	// Encoding file data
+	gob.Register(elliptic.P256())
+	encoder := gob.NewEncoder(&content)
+
+	err := encoder.Encode(ws)
+	if err != nil {
+		log.Panic(err)
+	}
+	err = ioutil.WriteFile(walletFile, content.Bytes(), 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+// LoadFile loads all Wallets from a saved file
+func (ws *Wallets) LoadFile() error {
+	// Checking file existence
+	if _, err := os.Stat(walletFile); os.IsNotExist(err) {
+		return err
+	}
+
+	var wallets Wallets
+
+	fileContent, err := ioutil.ReadFile(walletFile)
+	if err != nil {
+		return err
+	}
+
+	// Decoding file data
+	gob.Register(elliptic.P256())
+	decoder := gob.NewDecoder(bytes.NewReader(fileContent))
+
+	err = decoder.Decode(&wallets)
+	if err != nil {
+		return err
+	}
+
+	ws.Wallets = wallets.Wallets
+
+	return nil
+}
