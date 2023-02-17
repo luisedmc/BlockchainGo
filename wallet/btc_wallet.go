569583fb1b0efa57c89171a074eb2b54644a77ec
LuisEduardo-M <luiseduardomc019@gmail.com>
2023-02-16 22:44:49 -0300

    refactor: fix comments information

---
+++ wallet/btc_wallet.go
@@ -0,0 +1,75 @@
+package wallet
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"log"
+
+	"golang.org/x/crypto/ripemd160"
+)
+
+const (
+	checkSumLength = 4
+	version        = byte(0x00)
+)
+
+// Wallet holds the Private Key and Public Key, to sign transactions and verify the signature, respectively
+type Wallet struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey  []byte
+}
+
+// CreateWallet creates a new Wallet by generating a new Private and Public key
+func CreateWallet() *Wallet {
+	private, public := generateKeyPair()
+
+	wallet := Wallet{
+		PrivateKey: private,
+		PublicKey:  public,
+	}
+
+	return &wallet
+}
+
+// PublicKeyHash hashes twice the Public Key using RIPEMD160 and SHA-256 algorithms
+func PublicKeyHash(publicKey []byte) []byte {
+	publicSHA256 := sha256.Sum256(publicKey)
+
+	hasherRIPEMD160 := ripemd160.New()
+
+	_, err := hasherRIPEMD160.Write(publicSHA256[:])
+	if err != nil {
+		log.Panic(err)
+	}
+
+	publicRIPEMD160 := hasherRIPEMD160.Sum(nil)
+
+	return publicRIPEMD160
+}
+
+// generateKeyPair generates a new Public and Private key
+func generateKeyPair() (ecdsa.PrivateKey, []byte) {
+	// secp256r1 curve also knows as P-256
+	curve := elliptic.P256()
+
+	// Generating Private Key with ECDSA algorithm
+	privateKey, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// Private Key derives Public Key
+	publicKey := append(privateKey.PublicKey.X.Bytes(), privateKey.PublicKey.Y.Bytes()...)
+
+	return *privateKey, publicKey
+}
+
+// checkSum generates a new checksum used to prevent errors in wallets addresses
+func checkSum(payload []byte) []byte {
+	firstHash := sha256.Sum256(payload)
+	secondHash := sha256.Sum256(firstHash[:])
+
+	return secondHash[:checkSumLength]
+}
